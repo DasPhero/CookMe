0f48dc470ade74a4b61a2913e9471839b1361622
LAPTOP-Phero <Niko.Samson@web.de>
2018-03-06 17:49:13 +0100

    logout button

--- Dynamic Prototype/Cook3/WebContent/scripts/login.js
+++ Dynamic Prototype/Cook3/WebContent/scripts/login.js
@@ -72,3 +72,7 @@ getAndValidateCookie = () => {
 
 	return cookieValue;
 }
+
+var deleteCookie = function(cookieName) {
+    document.cookie = cookieName + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
